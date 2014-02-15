e1bc00fce988be976daf1433ff00676af1910f18
gokhankcmn@gmail.com <gokhankcmn@gmail.com@6af2d8a7-0275-ff13-229b-bbaf30319c00>
2014-02-15 12:39:58 +0000

    kullanici ile ilgili servisler...

---
+++ trunk/booklet-ws/booklet-ws/src/main/java/com/fagose/booklet/service/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.fagose.booklet.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fagose.booklet.dao.UserDao;
+import com.fagose.booklet.dao.UserRoleDao;
+import com.fagose.booklet.model.User;
+import com.fagose.booklet.model.Userroles;
+
+@Service("userService")
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+public class UserServiceImpl implements UserService {
+
+        @Autowired
+        private UserDao userDao;
+        
+        @Autowired
+        private UserRoleDao userRoleDao;
+
+        public UserServiceImpl() {
+        }
+
+		@Override
+		public User getUserRole(String username, String password) {
+			// TODO Auto-generated method stub
+			User user=userDao.getUserbyUsernamePassword(username, password);
+			if(user==null){
+				//throw new BadCredentialsException("username and password is required");
+				return null;
+			}
+			else{
+			    Userroles userRole=userRoleDao.getUserRolebyUserId(user.getUserId());
+				if(userRole==null){
+					return null;
+				}
+				else{
+					if(userRole.getRoleName().equals("ROLE_ADMIN")){
+						return user;
+					}else{
+						return null;
+					}
+				}
+			}
+			
+		}
+
+}
+
