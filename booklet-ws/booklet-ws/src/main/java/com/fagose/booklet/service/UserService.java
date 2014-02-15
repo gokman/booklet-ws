c7645689023e09f197420e51a9a2c681a4a7b3bb
gokhankcmn@gmail.com <gokhankcmn@gmail.com@6af2d8a7-0275-ff13-229b-bbaf30319c00>
2014-02-15 12:39:58 +0000

    kullanici ile ilgili servisler...

---
+++ booklet-ws/booklet-ws/src/main/java/com/fagose/booklet/service/UserService.java
@@ -0,0 +1,9 @@
+package com.fagose.booklet.service;
+
+import com.fagose.booklet.model.User;
+
+public interface UserService {
+
+        public User getUserRole(String username,String password);
+}
+
